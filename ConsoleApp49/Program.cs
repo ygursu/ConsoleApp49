5aed9e6156a3ee74af34eb69399bcdfdd495f837
ygursu <yasemingrs@gmail.com>
2024-02-08 01:49:17 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp49/Program.cs
@@ -0,0 +1,26 @@
+﻿private static double Ortalama(int[] sayilar)
+{
+    int toplam = 0;
+    for (int i = 0; i < sayilar.Length; i++)
+    {
+        toplam += sayilar[i];
+    }
+
+    return (double)toplam / sayilar.Length;
+}
+static void Main(string[] args)
+{
+
+    Random rastgele = new Random();
+
+    int[] sayilar = new int[10];
+
+    for (int i = 0; i < sayilar.Length; i++)
+    {
+        sayilar[i] = rastgele.Next(100);
+    }
+
+    Console.Write("Dizideki Sayıların Ortalaması:" + Ortalama(sayilar));
+
+    Console.ReadKey();
+}
